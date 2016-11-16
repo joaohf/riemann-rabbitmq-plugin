0856fedab759edae5735ef722935c63e65f53c50
João Henrique Ferreira de Freitas <joaohf@gmail.com>
2016-11-15 23:27:25 -0200

    Add support to create rpm and tar packages

--- project.clj
+++ project.clj
@@ -4,7 +4,19 @@
                  [com.novemberain/langohr "3.1.0" :exclusions [cheshire clj-http]]
                  ]
   :plugins [[codox "0.6.1"]
-            [lein-midje "3.0.0"]]
+            [lein-midje "3.0.0"]
+            [lein-rpm "0.0.5"
+             :exclusions [org.apache.maven/maven-plugin-api
+                          org.codehaus.plexus/plexus-container-default
+                          org.codehaus.plexus/plexus-utils
+                          org.clojure/clojure
+                          classworlds]]
+            ; for lein-rpm
+            [org.apache.maven/maven-plugin-api "2.0"]
+            [org.codehaus.plexus/plexus-container-default
+             "1.0-alpha-9-stable-1"]
+            [org.codehaus.plexus/plexus-utils "1.5.15"]
+            [classworlds "1.1"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]
                                   [riemann "0.2.10"]
                                   ]}}
